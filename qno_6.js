ccc369a3f1a25ff2f6ca0865159414d7cb4c3f51
Bidhan Khatri <khatribidhan9@gmail.com>
2024-12-15 11:36:08 +0545

    alternative way added

--- qno_6.js
+++ qno_6.js
@@ -9,3 +9,19 @@ return result;
 }
 
 console.log(reverseString(str));
+
+
+//Alternative way
+
+// function reverseString(str){
+
+//     let reversedStr = '';
+
+//     for(let i = str.length - 1; i >= 0; i--){
+        
+//         reversedStr += str[i];
+//     }
+//     return reversedStr;
+
+// }
+// console.log(reverseString(str));
