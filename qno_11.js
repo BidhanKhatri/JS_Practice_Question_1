20bf4d8559703d329d990a28eef9aaa51fcb002e
Bidhan Khatri <khatribidhan9@gmail.com>
2024-12-18 19:26:06 +0545

    qno11 added

---
+++ qno_11.js
@@ -0,0 +1,13 @@
+//Write a function that returns the result of raising a given number to specified power
+
+function raiseToPower(base,exponent){
+   
+    let result = 1;
+    for(let i = 1; i <= exponent; i++){
+        result *= base;
+    }
+    return result;
+
+}
+
+console.log(raiseToPower(2,3));
