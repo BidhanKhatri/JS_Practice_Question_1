a0a00832b5d3311b3579cc26cb6b9db0a00e03f6
Bidhan Khatri <khatribidhan9@gmail.com>
2024-12-18 19:40:57 +0545

    qno11 added

--- qno_11.js
+++ qno_11.js
@@ -1,4 +1,4 @@
-//Write a function that returns the result of raising a given number to specified power
+//Write a function that returns the result of raising a given number to specified power (logical one)
 
 function raiseToPower(base,exponent){
    
